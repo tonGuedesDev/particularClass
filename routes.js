9e48f4b4256588dea5c4c22c6a7768299844c9ba
kerity <naoesobrevencer@gmail.com>
2021-06-23 14:54:30 -0300

    :fire: add card

--- routes.js
+++ routes.js
@@ -2,7 +2,7 @@ const express = require("express")
 const routes = express.Router()
 
 routes.get("/", (req, res) => {
-    res.render("layout")
+    res.render("teachers")
 })
 
 module.exports = routes
