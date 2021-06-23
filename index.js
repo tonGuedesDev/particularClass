c804aa0b0203e7bf6c8b2304fe71f0ca005b1c65
kerity <naoesobrevencer@gmail.com>
2021-06-23 01:20:20 -0300

    :fire: add header

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+
+const server = express()
+const routes = require("./routes")
+const nunjucks = require('nunjucks')
+
+nunjucks.configure("view ")
+
+server.use(express.static("public"))
+server.use(routes)
+
+server.set("view engine", "njk")
+nunjucks.configure("views", {
+    express: server
+})
+
+server.listen(9999, () => {
+    console.log('server running')
+})
